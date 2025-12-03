271ca1fc597f5648349bc390764031ff3e184cbf
Ude Desire <udesire20@gmail.com>
2025-12-03 14:56:55 +0100

    jS CALCULATOR COMMENT

--- public/script.js
+++ public/script.js
@@ -29,6 +29,13 @@ numberBtns.forEach(button => {
     });
 });
 
+clearBtns.addEventListener('click', function() {
+    currentInput = '0';
+    previousInput = '';
+    operation = null;
+    display.value = currentInput;
+});
+
 deleteBtns.addEventListener('click', function() {
     // Remove last character
     currentInput = currentInput.slice(0, -1);
@@ -40,3 +47,43 @@ deleteBtns.addEventListener('click', function() {
     
     display.value = currentInput;
 });
+
+operatorBtns.forEach(button => {
+    button.addEventListener('click', function() {
+        const op = button.getAttribute('data-operator');
+        
+        previousInput = currentInput;
+        currentInput = '0';
+        operation = op;
+    });
+});
+
+equalsBtns.addEventListener('click', function() {
+    if (operation === null) return;
+    
+    const num1 = Number(previousInput);
+    const num2 = Number(currentInput);
+    let result = 0;
+    
+    if (operation === '+') {
+        result = num1 + num2;
+    } else if (operation === '-') {
+        result = num1 - num2;
+    } else if (operation === '*') {
+        result = num1 * num2;
+    } else if (operation === '/') {
+        if (num2 === 0) {
+            result = 'Error';
+        } else {
+            result = num1 / num2;
+        }
+    }
+    
+    currentInput = result.toString();
+    operation = null;
+    previousInput = '';
+    display.value = currentInput;
+});
+
+
+
